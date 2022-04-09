02f068363fc05eb1a1f8f792b2b7f556e5b287ff
gustavo <gustavo.kuno@linx.com.br>
2022-04-09 13:56:08 -0300

    add docker role.

--- how-to-run.sh
+++ how-to-run.sh
@@ -1,10 +1,14 @@
  #!/bin/sh
- 
-docker run -v ${pwd}:/work:ro --env-file ${pwd}/.env.local --rm gustavmk/ansiblecontainer:latest "./auth_azcli.sh ; ansible-inventory -i inventory/in-azure_rm.yml --list"
 
 
-docker run -v ${pwd}:/work:ro --env-file ${pwd}/.env.local -v   ${pwd}/roles:/root/.ansible/roles:ro --rm gustavmk/ansiblecontainer:latest "./auth_azcli.sh ; ansible-playbook -i inventory/in-azure_rm.yml myRole.yml"
+# Check inventory - Both Linux / Windows
+docker run -v ${PWD}:/work:ro --env-file ${PWD}/.env.local --rm gustavmk/ansiblecontainer:latest "./auth_azcli.sh ; ansible-inventory -i inventory/in-azure_rm.yml --list" 
+docker run -v ${PWD}:/work:ro --env-file ${PWD}/.env.local --rm gustavmk/ansiblecontainer:latest "./auth_azcli.sh ; ansible-inventory -i inventory/in-azure_rm.yml --graph" 
 
 
-# IIS Role
-docker run -v ${pwd}:/work:ro --env-file ${pwd}/.env.local -v  ${pwd}/roles:/root/.ansible/roles:ro --rm gustavmk/ansiblecontainer:latest "./auth_azcli.sh ; ansible-playbook -i inventory/in-azure_rm.yml play-win-role-app1.yml"
+
+# linux - Docker role
+ docker run -v ${PWD}:/work:ro --env-file ${PWD}/.env.linux.local -v  ${PWD}/roles:/root/.ansible/roles:ro --rm gustavmk/ansiblecontainer:latest "./auth_azcli.sh ; ansible-playbook -i inventory/in-azure_rm.yml play-linux-role-docker.yml" 
+
+# Windows - IIS Role
+docker run -v ${PWD}:/work:ro --env-file ${PWD}/.env.local -v  ${PWD}/roles:/root/.ansible/roles:ro --rm gustavmk/ansiblecontainer:latest "./auth_azcli.sh ; ansible-playbook -i inventory/in-azure_rm.yml play-win-role-app1.yml"

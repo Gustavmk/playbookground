39110b0d9254c9f25c2be8201a751513f9a9663a
gustavo <gustavo.kuno@linx.com.br>
2022-04-11 13:30:00 -0300

    add win-ssh role

--- how-to-run.sh
+++ how-to-run.sh
@@ -14,3 +14,7 @@ docker run -v ${PWD}:/work:ro --env-file ${PWD}/.env.local --rm gustavmk/ansible
 
 # Windows - IIS Role
 docker run -v ${PWD}:/work:ro --env-file ${PWD}/.env.local -v  ${PWD}/roles:/root/.ansible/roles:ro --rm gustavmk/ansiblecontainer:latest "./auth_azcli.sh ; ansible-playbook -i inventory/in-azure_rm.yml play-win-role-app1.yml"
+
+
+# Windows - SSH Role
+docker run -v ${PWD}:/work:ro --env-file ${PWD}/linx.env.local -v  ${PWD}/roles:/root/.ansible/roles:ro --rm gustavmk/ansiblecontainer:latest "ansible-playbook -i inventory/00.inventory play-win-ssh.yml"

a73c38f302d5dbc56dd54e95238bf02bbf7bb950
gustavo <gustavo.kuno@linx.com.br>
2022-04-12 20:07:07 -0300

    .

--- how-to-run.sh
+++ how-to-run.sh
@@ -1,13 +1,14 @@
  #!/bin/sh
 
-
-# Check inventory - Both Linux / Windows
+# Check Dynamic  Inventory - Both Linux / Windows
 docker run -v ${PWD}:/work:ro --env-file ${PWD}/.env.local --rm gustavmk/ansiblecontainer:latest "./auth_azcli.sh ; ansible-inventory -i inventory/in-azure_rm.yml --list" 
 docker run -v ${PWD}:/work:ro --env-file ${PWD}/.env.local --rm gustavmk/ansiblecontainer:latest "./auth_azcli.sh ; ansible-inventory -i inventory/in-azure_rm.yml --graph" 
 
+# Check Inventory - Both Linux / Windows
+docker run -v ${PWD}:/work:ro --env-file ${PWD}/.env.local --rm gustavmk/ansiblecontainer:latest "ansible-inventory -i inventory/00.inventory --list" 
+
 # Create a new role - Both
 docker run -v ${PWD}:/work:ro --env-file ${PWD}/.env.local --rm gustavmk/ansiblecontainer:latest "ansible-galaxy init ${PWD}/tools"
-
 
 # linux - Docker role
 docker run -v ${PWD}:/work:ro --env-file ${PWD}/.env.linux.local -v  ${PWD}/roles:/root/.ansible/roles:ro --rm gustavmk/ansiblecontainer:latest "./auth_azcli.sh ; ansible-playbook -i inventory/in-azure_rm.yml play-linux-role-docker.yml" 
@@ -15,10 +16,8 @@ docker run -v ${PWD}:/work:ro --env-file ${PWD}/.env.linux.local -v  ${PWD}/role
 # Linux - Swarm Traefik
 docker run -v ${PWD}:/work:ro --env-file ${PWD}/.env.linux.local -v  ${PWD}/roles:/root/.ansible/roles:ro --rm gustavmk/ansiblecontainer:latest "./auth_azcli.sh ; ansible-playbook -i inventory/in-azure_rm.yml play-linux-role-swarm-traefik.yml" 
 
-
 # Windows - IIS Role
 docker run -v ${PWD}:/work:ro --env-file ${PWD}/.env.local -v  ${PWD}/roles:/root/.ansible/roles:ro --rm gustavmk/ansiblecontainer:latest "./auth_azcli.sh ; ansible-playbook -i inventory/in-azure_rm.yml play-win-role-app1.yml"
-
 
 # Windows - SSH Role
 docker run -v ${PWD}:/work:ro --env-file ${PWD}/linx.env.local -v  ${PWD}/roles:/root/.ansible/roles:ro --rm gustavmk/ansiblecontainer:latest "ansible-playbook -i inventory/00.inventory play-win-ssh.yml"

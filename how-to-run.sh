c7c440c23fdf6a030cd0967028164ec3f0dbaa24
gustavo <gustavo.kuno@linx.com.br>
2022-04-13 11:59:44 -0300

    .

--- how-to-run.sh
+++ how-to-run.sh
@@ -10,6 +10,13 @@ docker run -v ${PWD}:/work:ro --env-file ${PWD}/.env.local --rm gustavmk/ansible
 # Windows - WIN_PING 
 docker run -v ${PWD}:/work:ro --env-file ${PWD}/.env.local --rm gustavmk/ansiblecontainer:latest "ansible-playbook -i inventory/00.inventory play-win-ping.yml"
 
+
+docker run -v ${PWD}:/work:ro --env-file ${PWD}/.env.local --rm gustavmk/ansiblecontainer:latest "./auth_azcli.sh ; ansible-playbook -i inventory/in-azure_rm.yml play-win-ping.yml"
+
+# Windows - SSH Role
+docker run -v ${PWD}:/work:ro --env-file ${PWD}/env.local -v  ${PWD}/roles:/root/.ansible/roles:ro --rm gustavmk/ansiblecontainer:latest "ansible-playbook -i inventory/00.inventory play-win-ssh.yml"
+
+
 # Create a new role - Both
 docker run -v ${PWD}:/work:ro --env-file ${PWD}/.env.local --rm gustavmk/ansiblecontainer:latest "ansible-galaxy init ${PWD}/tools"
 
@@ -21,6 +28,3 @@ docker run -v ${PWD}:/work:ro --env-file ${PWD}/.env.linux.local -v  ${PWD}/role
 
 # Windows - IIS Role
 docker run -v ${PWD}:/work:ro --env-file ${PWD}/.env.local -v  ${PWD}/roles:/root/.ansible/roles:ro --rm gustavmk/ansiblecontainer:latest "./auth_azcli.sh ; ansible-playbook -i inventory/in-azure_rm.yml play-win-role-app1.yml"
-
-# Windows - SSH Role
-docker run -v ${PWD}:/work:ro --env-file ${PWD}/linx.env.local -v  ${PWD}/roles:/root/.ansible/roles:ro --rm gustavmk/ansiblecontainer:latest "ansible-playbook -i inventory/00.inventory play-win-ssh.yml"
